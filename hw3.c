4ce64430cc097bdecda1c570016fcb9f1e9de0ab
Theodore Peters <tpeters@stuy.edu>
2018-09-19 21:20:34 -0400

    but homework get done at home

--- hw3.c
+++ hw3.c
@@ -1,4 +1,5 @@
 #include <stdlib.h>
+#include <stdio.h>
 #include <time.h>
 
 int main() {
@@ -7,5 +8,17 @@ int main() {
   for (int i = 0; i < 10; i++)
     a[i] = rand();
   a[9] = 0;
+  
+  printf("wacky array: ");
+  for (int i = 0; i < 10; i++)
+    printf("%d ", a[i]);
+  
+  printf("\nwacky array but now its backwards: ");
+  int b[10];
+  for (int i = 0; i < 10; i++) {
+    *(b + i) = *(a + 9 - i);
+    printf("%d ", *(b + i));
+  }
+  
   return 0;
 }
